22ead6d6edb5c7feb7255491b5b8e94a13583e03
Slim Arfaoui <Slim Arfaoui@localhost>
2012-03-09 15:02:06 +0100

    Url encoding methods

---
+++ ScrapySharp.Tests/When_using_url_utility.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using ScrapySharp.Utilities;
+
+namespace ScrapySharp.Tests
+{
+    [TestFixture]
+    public class When_using_url_utility
+    {
+        [Test]
+        public void When_url_encoding_string_0()
+        {
+            Assert.That(UrlUtility.Encode(" "), Is.EqualTo("%20"));
+        }
+
+        [Test]
+        public void When_url_encoding_string_1()
+        {
+            Assert.That(UrlUtility.Encode("\"Aardvarks lurk, OK?\""), Is.EqualTo("%22Aardvarks%20lurk%2C%20OK%3F%22"));
+        }
+
+        [Test]
+        public void When_url_decoding_string_1()
+        {
+            Assert.That(UrlUtility.Decode("%22Aardvarks%20lurk%2C%20OK%3F%22"), Is.EqualTo("\"Aardvarks lurk, OK?\""));
+        }
+
+        [Test]
+        public void When_detecting_decoded_string_1()
+        {
+            Assert.IsFalse(UrlUtility.IsEncoded("\"Aardvarks lurk, OK?\""));
+        }
+
+        [Test]
+        public void When_detecting_encoded_string_1()
+        {
+            Assert.IsTrue(UrlUtility.IsEncoded("%22Aardvarks%20lurk%2C%20OK%3F%22"));
+        }
+
+        [Test]
+        public void When_detecting_encoded_string_2()
+        {
+            Assert.IsFalse(UrlUtility.IsEncoded("%22Aardvarks%20lurk%2C%20OK%3F%22%0"));
+        }
+    }
+}
---
+++ ScrapySharp/Utilities/UrlUtility.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+namespace ScrapySharp.Utilities
+{
+    public static class UrlUtility
+    {
+        private const string UrlEncodeUnreservedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private const string Hexa = "0123456789ABCDEFabcdef";
+
+        public static bool IsEncoded(string value)
+        {
+            for (var i = 0; i < value.Length; i++)
+            {
+                var c = value[i];
+                if (c == '%')
+                {
+                    if (i + 2 >= value.Length || !Hexa.Contains(value[i + 1]) || !Hexa.Contains(value[i + 2]))
+                        return false;
+                    i += 2;
+                    continue;
+                }
+                if (!UrlEncodeUnreservedChars.Contains(c))
+                    return false;
+            }
+            return true;
+        }
+
+        public static string Encode(string value)
+        {
+            return
+                value.Aggregate(string.Empty,
+                                (acc, cur) =>
+                                acc +
+                                (UrlEncodeUnreservedChars.Contains(cur)
+                                     ? cur.ToString()
+                                     : string.Format("%{0:x2}", (byte) cur).ToUpper()));
+        }
+
+        public static string Decode(string value)
+        {
+            var decoded = string.Empty;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                var c = value[i];
+                if (c == '%' && (i + 2 < value.Length && Hexa.Contains(value[i + 1]) && Hexa.Contains(value[i + 2])))
+                {
+                    var left = Hexa.IndexOf(value[i + 1].ToString().ToUpper());
+                    var right = Hexa.IndexOf(value[i + 2].ToString().ToUpper());
+
+                    decoded += (char)((left << 4) | right);
+                    i += 2;
+                    continue;
+                }
+                decoded += c;
+            }
+
+            return decoded;
+        }
+    }
+}
