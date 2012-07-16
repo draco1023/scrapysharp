c41d2b09dc31b23d40c81d45e23b8f37b28552ff
Romain Flechner <Romain Flechner@localhost>
2012-07-16 12:21:45 +0200

    Create Benchmarks project

---
+++ ScrapySharp.Benchmarks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ScrapySharp.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var agilityPackBenchMark = new AgilityPackBenchMark();
+            var hDocumentBenchMark = new HDocumentBenchMark();
+
+            agilityPackBenchMark.Run();
+            Console.WriteLine("AgilityPackBenchMark => Elapsed time: {0} ms", agilityPackBenchMark.TimeElapsed.TotalMilliseconds);
+            
+            hDocumentBenchMark.Run();
+            Console.WriteLine("HDocumentBenchMark => Elapsed time: {0} ms", hDocumentBenchMark.TimeElapsed.TotalMilliseconds);
+            
+            
+            agilityPackBenchMark.Run();
+            Console.WriteLine("AgilityPackBenchMark => Elapsed time: {0} ms", agilityPackBenchMark.TimeElapsed.TotalMilliseconds);
+            
+            hDocumentBenchMark.Run();
+            Console.WriteLine("HDocumentBenchMark => Elapsed time: {0} ms", hDocumentBenchMark.TimeElapsed.TotalMilliseconds);
+            
+
+            agilityPackBenchMark.Run();
+            Console.WriteLine("AgilityPackBenchMark => Elapsed time: {0} ms", agilityPackBenchMark.TimeElapsed.TotalMilliseconds);
+
+            hDocumentBenchMark.Run();
+            Console.WriteLine("HDocumentBenchMark => Elapsed time: {0} ms", hDocumentBenchMark.TimeElapsed.TotalMilliseconds);
+
+
+            Console.WriteLine("Press any key ...");
+            Console.ReadKey(true);
+        }
+    }
+}
