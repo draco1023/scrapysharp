a4fc1302dde32688c0070f3802dc69f60c807ab9
Romain Flechner <Romain Flechner@localhost>
2013-01-04 14:05:19 +0100

    Complete the web form parsing

--- ScrapySharp.Tests/When_use_web_forms.cs
+++ ScrapySharp.Tests/When_use_web_forms.cs
@@ -1,4 +1,9 @@
+using System.IO;
 using NUnit.Framework;
+using ScrapySharp.Html.Dom;
+using ScrapySharp.Html.Forms;
+using ScrapySharp.Extensions;
+using System.Linq;
 
 namespace ScrapySharp.Tests
 {
@@ -8,9 +13,24 @@ namespace ScrapySharp.Tests
         [Test]
         public void When_parsing_form()
         {
+            var source = File.ReadAllText("Html/WebFormPage.htm");
+            var html = HDocument.Parse(source);
+            
+            var webForm = new WebForm(html.CssSelect("form[name=TestForm]").Single());
 
+            Assert.AreEqual(5, webForm.FormFields.Count);
+        }
 
+        [Test]
+        public void When_parsing_form_with_agility_pack()
+        {
+            var source = File.ReadAllText("Html/WebFormPage.htm");
+            var html = source.ToHtmlNode();
 
+            var webForm = new WebForm(html.CssSelect("form[name=TestForm]").Single());
+
+            //Because HtmlAgilityPack fails the form parsing !
+            Assert.AreNotEqual(5, webForm.FormFields.Count);
         }
     }
 }
