b86d9ad1e6a4e53e57924cf2de2951de4483fa4d
rflechner <rflechner@localhost>
2013-03-20 20:11:33 +0100

    don't re download cached resources

--- ScrapySharp/Cache/WebResourceEntity.cs
+++ ScrapySharp/Cache/WebResourceEntity.cs
@@ -1,17 +1,11 @@
-using ProtoBuf;
-
 namespace ScrapySharp.Cache
 {
-    [ProtoContract]
     public class WebResourceEntity
     {
-        [ProtoMember(1)]
         public string LastModified { get; set; }
 
-        [ProtoMember(2)]
         public string AbsoluteUrl { get; set; }
 
-        [ProtoMember(3)]
         public bool ForceDownload { get; set; }
     }
 }
